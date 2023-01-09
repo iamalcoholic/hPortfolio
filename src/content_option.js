8ba411881b2641022fe51e5558cfe06882ae6ff1
sarintoxic <nguyentrunghieux24@gmail.com>
2023-01-09 12:58:21 +0300

    updtae
    sss

---
+++ src/content_option.js
@@ -0,0 +1,128 @@
+const logotext = "HIIU";
+const meta = {
+    title: "Hieu Nguyen",
+    description: "I’m Hieu Nguyen automation engineer _ Full stack devloper,currently working in Russia",
+};
+
+const introdata = {
+    title: "I’m Hieu Nguyen",
+    animated: {
+        first: "I love coding",
+        second: "I code cool websites",
+        third: "I develop apps",
+    },
+    description: "A Automation Engineer & A Web Designer / Developer Located In Russia",
+    your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
+};
+
+const dataabout = {
+    title: "abit about my self",
+    aboutme: "A Automation Engineer & A Web Designer / Developer Located In Russia",
+};
+const worktimeline = [{
+        jobtitle: "Co-Founder",
+        where: "CK Capital",
+        date: "2022",
+    },
+    {
+        jobtitle: "Researcher",
+        where: "Kuznetsov Naval Academy",
+        date: "2020",
+    },
+    {
+        jobtitle: "Web Designer",
+        where: "Horus",
+        date: "2019",
+    },
+];
+
+const skills = [{
+        name: "Python",
+        value: 80,
+    },
+    {
+        name: "C++",
+        value: 85,
+    },
+    {
+        name: "Javascript",
+        value: 80,
+    },
+    {
+        name: "React",
+        value: 80,
+    },
+    {
+        name: "Solidity",
+        value: 65,
+    },
+];
+
+const services = [{
+        title: "UI & UX Design",
+        description: "Responsible for the visual aspect of the interface and deciding the user’s journey, focuses on usability and what the user’s experience throughout the process.",
+    },
+    {
+        title: "Web3 Apps",
+        description: "Application built on a decentralized network (Ethereum, Binance Chain, Fantom Chain, Polygon...) that combines a smart contract and a frontend user interface.",
+    },
+    {
+        title: "Backend",
+        description: "API, data storage, security, and other server-side functions",
+    },
+];
+
+const dataportfolio = [{
+        img: "https://picsum.photos/400/?grayscale",
+        desctiption: "CK Capital Landing Page",
+        link: "http://ckcapital.org",
+    },
+    {
+        img: "https://picsum.photos/400/800/?grayscale",
+        desctiption: "Personal Blog",
+        link: "http://blog.hiiu.ru",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        desctiption: "Warship Assistant",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        desctiption: "PiVerse",
+        link: "#",
+    },
+    
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "contact@hiiu.ru",
+    YOUR_FONE: "+79811623986",
+    description: "Weekend only ",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com/sarintoxic",
+    facebook: "https://facebook.com/hieunguyenx24",
+    linkedin: "https://linkedin.com",
+    twitter: "https://twitter.com/hieunguyenx24",
+    youtube: "https://youtube.com",
+    twitch: "https://twitch.com",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
